c77481d6e0690309edbf6001a1bbaac449846321
JLong <www.1269152641@qq.com>
2023-07-03 22:28:44 +0800

    feat: 搭建effect方法

---
+++ src/reactivity/effect.ts
@@ -0,0 +1,46 @@
+class ReactiveEffect {
+    private _fn: any
+
+    constructor(fn: any) {
+        this._fn = fn
+    }
+    
+    run() {
+        activeEffect = this
+        this._fn()
+    }
+}
+
+const targetMap = new Map()
+export function track(target: any, key: any) {
+    // target -> key -> dep
+    let depsMap = targetMap.get(target)
+    if (!depsMap) {
+        depsMap = new Map()
+        targetMap.set(target, depsMap)
+    }
+    
+    let dep = depsMap.get(key)
+    if (!dep) {
+        dep = new Set()
+        depsMap.set(key, dep)
+    }
+
+    dep.add(activeEffect)
+}
+
+export function trigger(target: any, key: string | symbol) {
+    let depsMap = targetMap.get(target)
+    let dep = depsMap.get(key)
+
+    for(const effect of dep) {
+        effect.run()
+    }
+}
+
+let activeEffect: any // 副作用函数实例类
+export function effect(fn: any) {
+    // fn
+    const _effect = new ReactiveEffect(fn)
+    _effect.run()
+}
